033e7cfbbe439240031e232cf73baf7922c4d6cd
fsaiz97 <felixsaiz97@gmail.com>
2022-09-03 13:55:21 +0100

    - Renamed urls.txt to input.txt - The producer takes a file path instead of a file object - The consumer now puts its output in a file instead of printing to stdout

--- link_extractor.py
+++ link_extractor.py
@@ -1,5 +1,6 @@
 import requests
 import queue
+from threading import Thread
 
 from html.parser import HTMLParser
 
@@ -15,26 +16,27 @@ class HyperlinkParser(HTMLParser):
                 self.links.append(attr[1])
 
 
-def producer(shared_queue, input_file):
-    for line in input_file:
-        url = line.strip()
-        try:
-            print("Extracting HTML from", url)
-            extracted_data = requests.get(url).text
-        except requests.exceptions.MissingSchema:
-            print("Error:", url, "misshapen. Skipping")
-        else:
-            print("Done")
-            print("Queuing HTMl from", url)
-            shared_queue.put((url, extracted_data))
-            print("Done")
+def producer(shared_queue, input_path):
+    with open(input_path) as input_file:
+        for line in input_file:
+            url = line.strip()
+            try:
+                print("Extracting HTML from", url)
+                extracted_data = requests.get(url).text
+            except requests.exceptions.MissingSchema:
+                print("Error:", url, "misshapen. Skipping")
+            else:
+                print("Done")
+                print("Queuing HTMl from", url)
+                shared_queue.put((url, extracted_data))
+                print("Done")
 
     print("Producer done")
 
     shared_queue.put(None)
 
 
-def consumer(shared_queue):
+def consumer(shared_queue, output_file):
     while True:
         parser = HyperlinkParser()
         item = shared_queue.get()
@@ -43,13 +45,23 @@ def consumer(shared_queue):
 
         parser.feed(item[1])
 
-        print(item[0], ":\n", parser.links, sep='')
+        print(item[0], ":\n", parser.links, sep='', file=output_file)
 
     print("Consumer done")
 
 
 if __name__ == "__main__":
     html_queue = queue.Queue()
-    with open("input/urls.txt") as file:
-        producer(html_queue, file)
-    consumer(html_queue)
+    input_path = "files/input.txt"
+
+    producer = Thread(target=producer, args=(html_queue, input_path))
+    producer.start()
+
+    output_file = open("files/output.txt", 'w')
+    consumer = Thread(target=consumer, args=(html_queue, output_file))
+    consumer.start()
+
+    producer.join()
+    producer.join()
+
+    output_file.close()
--- test_link_extractor.py
+++ test_link_extractor.py
@@ -3,6 +3,8 @@ import queue
 from unittest.mock import patch, mock_open
 from io import StringIO
 import sys
+from random import random
+import os
 
 from link_extractor import producer, consumer, HyperlinkParser
 
@@ -28,8 +30,8 @@ class TestProducer(unittest.TestCase):
     @patch('link_extractor.requests.get', side_effect=mocked_requests_get)
     @patch("builtins.open", new_callable=mock_open, read_data="https://test.com\n")
     def test_producer_extracts_correctly(self, mocked_get, mocked_open):
-        with open("path/to/file") as file:
-            producer(self.html_queue, file)
+        fake_input_path = "path/to/file"
+        producer(self.html_queue, fake_input_path)
         self.assertEqual(fake_html, self.html_queue.get()[1])
 
 
@@ -48,19 +50,28 @@ class TestHyperlinkParser(unittest.TestCase):
 class TestConsumer(unittest.TestCase):
     def setUp(self):
         self.html_queue = queue.Queue()
-
-    def test_consumer_extracts_all_links(self):
-        output_trap = StringIO()
-        old_stdout = sys.stdout
-        sys.stdout = output_trap
         self.html_queue.put(("https://input.com", '<a href="https://test.com"><a href="https://google.com">'))
         self.html_queue.put(None)
+        while True:
+            random_num = str(int(random()*1000))
+            try:
+                self.output_file = open("tmp"+random_num, 'x+')
+            except FileExistsError:
+                continue
+            else:
+                break
 
-        consumer(self.html_queue)
-        sys.stdout = old_stdout
-        self.assertEqual("https://input.com:\n['https://test.com', 'https://google.com']\nConsumer done\n",
-                         output_trap.getvalue())
-        output_trap.close()
+    def tearDown(self):
+        output_file_name = self.output_file.name
+        self.output_file.close()
+        os.remove(output_file_name)
+
+    def test_consumer_extracts_all_links(self):
+        consumer(self.html_queue, self.output_file)
+        self.output_file.seek(0)
+
+        self.assertEqual("https://input.com:\n['https://test.com', 'https://google.com']\n",
+                         self.output_file.read())
 
 
 if __name__ == "__main__":
